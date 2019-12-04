956f774e328b6ab47a5a8c9334805ae29d1b967c
sediri05 <sehan.ediriweera@hotmail.com>
2019-12-04 16:51:33 +0000

    Everything done. Although grid chrome does not work along with the chrome headless download issue.

--- src/main/java/Framework/stepDefinitions/UserRegistrationSteps.java
+++ src/main/java/Framework/stepDefinitions/UserRegistrationSteps.java
@@ -13,22 +13,22 @@ public class UserRegistrationSteps extends DriverFactory {
 
     @Given("^User is on the Registration Page$")
     public void NavigateToRegistrationPage() {
-       /* tempObj.getNavBarActions().clickSignIn();
+        tempObj.getNavBarActions().clickSignIn();
         Log.info("Navigated to Sign In Page");
         tempObj.getUserRegistrationActions().enterEmail(prop.getProperty("registerEmail"));
         Log.info("Registration email entered");
         tempObj.getUserRegistrationActions().clickCreate();
-        Log.info("Create button clicked");*/
+        Log.info("Create button clicked");
     }
 
     @When("^The user enters their details on the registration page$")
     public void EnterDetails() {
-        /*tempObj.getUserRegistrationActions().enterLoginDetails(prop);
+        tempObj.getUserRegistrationActions().enterLoginDetails(prop);
         Log.info("Registration detailed are entered");
         tempObj.getUserRegistrationActions().enterAddressDetails(prop);
         Log.info("Address details are entered");
         tempObj.getUserRegistrationActions().enterPhone(prop.getProperty("MobPhone"));
-        Log.info("Phone Number is entered");*/
+        Log.info("Phone Number is entered");
     }
 
     @Then("^The User is able to register$")
@@ -38,22 +38,22 @@ public class UserRegistrationSteps extends DriverFactory {
     }
 
     @And("^Validate the details$")
-    public void Verify(){
-        /*tempObj.getmyAccActions().verifyRegisteredAndLoggedIn();
+    public void Verify() {
+        tempObj.getmyAccActions().verifyRegisteredAndLoggedIn();
         Log.info("Registration confirmation is verified and user is able to Log in");
 //        tempObj.getmyAccActions().verifyMyAccountPage();
         //Log.info("My Account Page displayed is verified");
         tempObj.getmyAccActions().navigateToPersonalInfoPage();
         Log.info("Navigated to personal info page");
         tempObj.getPersonalInfoActions().verifyDetails();
-        Log.info("Personal Info is verified");*/
+        Log.info("Personal Info is verified");
     }
 
     @And("^Logout$")
-    public void LogoutAndLogin(){
-        /*tempObj.getNavBarActions().clickSignOut();
+    public void LogoutAndLogin() {
+        tempObj.getNavBarActions().clickSignOut();
         Log.info("User is able to successfully Log out");
         tempObj.getUserRegistrationActions().Login();
-        Log.info("User is able to successfully Log in");*/
+        Log.info("User is able to successfully Log in");
     }
 }
